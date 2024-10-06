907f8dd0efde87f8585edea43d6b90a802186892
FilipJJJ <Filip.jarlesjo@gmail.com>
2024-10-06 22:58:37 +0200

    Updated header + mobile nav menu closes when item is pressed

--- components/customHeader.js
+++ components/customHeader.js
@@ -11,30 +11,36 @@ class CustomHeader extends HTMLElement
         this.innerHTML = `
         <header>
             
-            <nav class="navbar">
-                <a href="index.html" class="nav-logo"><h3>Filip Järlesjö</h3></a>
-                <ul class="nav-menu">
-                    <li class="nav-item">
-                        <a href="https://www.linkedin.com/in/filip-j%C3%A4rlesj%C3%B6-4a30692b3/" class="nav-link">LinkedIn <img src="Media/iconmonstr-linkedin-3.svg" alt="LinkedIn" class="nav-item-image"></img></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="https://github.com/Flippers17" class="nav-link">GitHub <img src="github-mark/github-mark.svg" alt="GitHub" class="nav-item-image"></img></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="mailto: filip.jarlesjo@gmail.com" class="nav-link">filip.jarlesjo@gmail.com <img src="Media/email-svgrepo-com.svg" alt="Gmail" class="nav-item-image"></img></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="Media/CV Filip Järlesjö.pdf" class="nav-link">CV</a>
-                    </li>
-                </ul>
-                <div class="hamburger">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                </div>
-            </nav>
-            
-        </header>
+        <nav class="navbar">
+            <a href="index.html" class="nav-logo"><h3>Filip Järlesjö</h3></a>
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <a href="#aboutMe" class="nav-link">About Me </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#gamesSection" class="nav-link">Games </a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://www.linkedin.com/in/filip-j%C3%A4rlesj%C3%B6-4a30692b3/" class="nav-link">LinkedIn <img src="Media/iconmonstr-linkedin-3.svg" alt="LinkedIn" class="nav-item-image"></img></a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://github.com/Flippers17" class="nav-link">GitHub <img src="github-mark/github-mark.svg" alt="GitHub" class="nav-item-image"></img></a>
+                </li>
+                <li class="nav-item">
+                    <a href="mailto: filip.jarlesjo@gmail.com" class="nav-link">filip.jarlesjo@gmail.com <img src="Media/email-svgrepo-com.svg" alt="Gmail" class="nav-item-image"></img></a>
+                </li>
+                <li class="nav-item">
+                    <a href="Media/CV Filip Järlesjö.pdf" class="nav-link">CV</a>
+                </li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+        
+    </header>
         `
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navItems = document.querySelectorAll(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
+for(var i = 0; i < navItems.length; i++)
+{
+    navItems[i].addEventListener("click", mobileMenu);
+}
 
 
 function mobileMenu() 
