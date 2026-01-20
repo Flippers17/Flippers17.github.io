3283c8e33ce37fb9e05774dc809781c7ab8d31d8
FilipJJJ <Filip.jarlesjo@gmail.com>
2026-01-20 20:59:37 +0100

    Added "Read more" in some places

--- gameInfo.js
+++ gameInfo.js
@@ -1,4 +1,6 @@
 var screenshots = document.querySelectorAll(".gridScreenshotContainer");
+var readMoreButtons = document.querySelectorAll(".readMoreButton");
+var showLessButtons = document.querySelectorAll(".showLessButton");
 
 var currentEnlarged;
 
@@ -8,6 +10,20 @@ for (let index = 0; index < screenshots.length; index++)
     const element = screenshots[index];
     element.addEventListener("click", function(){EnlargeImage(element)});
     // element.classList.toggle("preview");
+}
+
+for (let index = 0; index < readMoreButtons.length; index++) 
+{
+    const readMoreButton = readMoreButtons[index];
+    readMoreButton.addEventListener("click", function(){ShowOptionalText(readMoreButton)});
+
+    const showLessButton = showLessButtons[index];
+    showLessButton.addEventListener("click", function(){ShowOptionalText(showLessButton)});
+    // element.classList.toggle("preview");
+}
+
+function ShowOptionalText(button){
+    button.parentNode.classList.toggle("showOptionalText");
 }
 
 
