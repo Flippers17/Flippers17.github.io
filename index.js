951b5c6a642d8cdd757d3059d801f8e4bbf0e3da
FilipJJJ <Filip.jarlesjo@gmail.com>
2026-02-08 16:52:33 +0100

    Made page for Beyond Zero + adjustments on index page (added other projects and sorted games)

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ function mobileMenu()
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+// (function($) {
+// var element = $('#scrollingDiv'),
+//     originalY = element.offset().top;
+
+// var socialelement =  $('#social-area'),
+//     limitY = socialelement.position().top;
+
+// // Space between element and top of screen (when scrolling)
+// var topMargin = 40;
+
+// // Should probably be set in CSS; but here just for emphasis
+// element.css('position', 'relative');
+
+// $(window).on('scroll', function(event) {
+//     var scrollTop = $(window).scrollTop();
+//     var CurrentY = scrollTop + element.outerHeight(true) + topMargin;   
+//     var MoveUp = limitY < CurrentY ? CurrentY - limitY : 0 ;
+
+//         element.stop(false, false).animate({
+//             top: scrollTop < originalY
+//                     ? 0
+//                     : scrollTop - originalY + topMargin - (MoveUp / 2)
+//         }, 500);
+// });
+
+// })(jQuery);
